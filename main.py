f1137bd99af6bbe4bcf73af8164bab8362f0249f
Mu'tasim <mmuhib136@gmail.com>
2021-10-15 22:57:56 -0400

    Code clean up with base main.py file.

--- main.py
+++ main.py
@@ -3,7 +3,13 @@ import sys, os, json
 import requests
 
 
+
+def write_data(json_data):
+    """ Function will write json to filename. """
+
+
 def get_url(tick: str):
+    """ Takes a stock tick and makes request aganist finance API """
     user_agent_headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
     base_url = "https://query2.finance.yahoo.com"
@@ -12,7 +18,6 @@ def get_url(tick: str):
     r = requests.get(url, headers=user_agent_headers)
     response = r.json()
     return response    
-
 
 
 def main():
